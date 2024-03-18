67998e789ba3fe8dd32bb8b34323a60e32fa10fa
Joshua Cuneo <jmcuneo@wpi.edu>
2024-03-18 10:55:40 -0400

    Replaced single quotes with double quotes. Minor formatting fixes.

--- public/js/main.js
+++ public/js/main.js
@@ -7,18 +7,18 @@ const submit = async function( event ) {
   // remains to this day
   event.preventDefault()
   
-  const input = document.querySelector( '#yourname' ),
+  const input = document.querySelector( "#yourname" ),
         json = { yourname: input.value },
         body = JSON.stringify( json )
 
-  const response = await fetch( '/submit', {
-    method:'POST',
+  const response = await fetch( "/submit", {
+    method:"POST",
     body 
   })
 
   const text = await response.text()
 
-  console.log( 'text:', text )
+  console.log( "text:", text )
 }
 
 window.onload = function() {
--- server.improved.js
+++ server.improved.js
@@ -1,23 +1,23 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
+const http = require( "http" ),
+      fs   = require( "fs" ),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
+      // to install the mime library if you"re testing this on your local machine.
       // However, Glitch will install it automatically by looking in your package.json
       // file.
-      mime = require( 'mime' ),
-      dir  = 'public/',
+      mime = require( "mime" ),
+      dir  = "public/",
       port = 3000
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  { "model": "toyota", "year": 1999, "mpg": 23 },
+  { "model": "honda", "year": 2004, "mpg": 30 },
+  { "model": "ford", "year": 1987, "mpg": 14} 
 ]
 
 const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
+  if( request.method === "GET" ) {
     handleGet( request, response )    
-  }else if( request.method === 'POST' ){
+  }else if( request.method === "POST" ){
     handlePost( request, response ) 
   }
 })
@@ -25,27 +25,27 @@ const server = http.createServer( function( request,response ) {
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
+  if( request.url === "/" ) {
+    sendFile( response, "public/index.html" )
   }else{
     sendFile( response, filename )
   }
 }
 
 const handlePost = function( request, response ) {
-  let dataString = ''
+  let dataString = ""
 
-  request.on( 'data', function( data ) {
+  request.on( "data", function( data ) {
       dataString += data 
   })
 
-  request.on( 'end', function() {
+  request.on( "end", function() {
     console.log( JSON.parse( dataString ) )
 
     // ... do something with the data here!!!
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
+    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
+    response.end("test")
   })
 }
 
@@ -54,18 +54,18 @@ const sendFile = function( response, filename ) {
 
    fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we've loaded the file successfully
+     // if the error = null, then we"ve loaded the file successfully
      if( err === null ) {
 
        // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
+       response.writeHeader( 200, { "Content-Type": type })
        response.end( content )
 
      }else{
 
        // file not found, error code 404
        response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+       response.end( "404 Error: File Not Found" )
 
      }
    })
